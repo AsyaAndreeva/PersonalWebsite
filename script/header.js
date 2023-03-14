52b5bc31df1bd8371286a44a81f785c9c4ac6969
Asya Andreeva <asiara032@gmail.com>
2023-03-14 10:01:08 +0200

    Fixed navigation

--- script/header.js
+++ script/header.js
@@ -1,21 +1,23 @@
-/*const sectionAll = document.querySelectorAll('section[id]');
+const sectionAll = document.querySelectorAll('section[id]');
+
 window.addEventListener('scroll',()=>{
-    const scrollY = window.pageYoffset;
+    const scrollY = window.pageYOffset;
+
     sectionAll.forEach((current)=>{
         const sectionHeight = current.offsetHeight;
         const sectionTop = current.offsetTop-100;
         const sectionId = current.getAttribute('id');
-        console.log(sectionId);
+
         if(scrollY > sectionTop && scrollY <= sectionTop+sectionHeight){
             document
-            .querySelector('li a[href*='+sectionId+'"]')
+            .querySelector('li a[href*='+sectionId+']')
             .classList.add('PrimeNavStyle');
         }
         else{
             document
-            .querySelector('li a[href*='+sectionId+'"]')
+            .querySelector('li a[href*='+sectionId+']')
             .classList.remove('PrimeNavStyle');
         }
     });
 });
-*/
+
