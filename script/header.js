bc9b67961d9510c9aaa4904e3c754b0f53620f8e
Asya Andreeva <117649237+AsyaAndreeva@users.noreply.github.com>
2024-01-14 12:46:58 +0200

    Added title animation with gsap

---
+++ script/header.js
@@ -0,0 +1,22 @@
+let text = document.getElementById("title");
+let originalText = text.textContent;
+let textArray = ['Web Developer', 'Student teacher at SoftUni Kids', 'UI & UX Designer'];
+let index = 0;
+
+function toggleText() {
+  let targetText = textArray[index];
+
+  gsap.to(text, {
+    duration: 1.5,
+    text: targetText,
+    ease: "none",
+  });
+    index++;
+    console.log(index);
+    if (index >= textArray.length) {
+        index = 0; // Reset the index when reaching the end of the array
+      }
+}
+
+// Call toggleText() every 5 seconds
+setInterval(toggleText, 3000);
